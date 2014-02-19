c9fb527a4032e49c6d0936feb689b203c8705108
Thales Macedo <thalesrm@gmail.com>
2014-02-19 18:05:44 +1100

    Animation js/css updates
    - Fixed the Experiences RollOver
    - added Header

--- js/animation.js
+++ js/animation.js
@@ -8,24 +8,23 @@
 function init() {
 
 	// Background
-	TweenLite.from("#blurred-image-container", 1, {opacity:.5, ease:Power2.easeOut});
+	//TweenLite.from("#blurred-image-container", 1, {opacity:.5, ease:Power2.easeOut});
 
 	// Logo
 	TweenLite.from("#logo", 1, {y:60, ease:Power2.easeOut, delay:.2});
 	TweenLite.from("#logo", 1.3, {opacity:0, ease:Power2.easeOut, delay:.2});
 
 	// H2 animation
-	TweenLite.fromTo(".header h2 .first", 1, {opacity:0, scaleX:0, rotation:10}, {opacity:1, scaleX:1, rotation:.5, ease:Power2.easeOut, delay:.5});
-
-	TweenLite.fromTo(".header h2 .second", 1,
-		/* from */{opacity:0, scaleX:0, rotation:10},
-		/* to */{opacity:1, scaleX:1, rotation:-4,
-		ease:Power2.easeOut, delay:.8});
+//	TweenLite.fromTo(".cover h2 .first", 1, {opacity:0, scaleX:0, rotation:10}, {opacity:1, scaleX:1, rotation:.5, ease:Power2.easeOut, delay:.5});
+//	TweenLite.fromTo(".cover h2 .second", 1,
+//		/* from */{opacity:0, scaleX:0, rotation:10},
+//		/* to */{opacity:1, scaleX:1, rotation:-4,
+//		ease:Power2.easeOut, delay:.8});
 
 
 	// Scroll btn
 
-	TweenLite.from("a.scroll", .5, {opacity:0, ease:Expo.easeInOut, delay:1.4});
+	//TweenMax.from("a.scroll", .5, {opacity:0, ease:Expo.easeInOut, delay:1.4});
 
 	TweenMax.fromTo("a.scroll #arrow", .5,
 		/* from */{y:-15},
@@ -35,19 +34,54 @@ function init() {
 
 /**************************************************
 
-					Animation for Experiences
+				Super Scrollorama / Parallax
 
 **************************************************/
-//javascript
+//Update all elements when window is resized
+$(window).resize(function () {
+	controller.triggerCheckAnim();
+});
 
-//GSAP 1.8.0
+$(document).ready(function() {
+	var controller = $.superscrollorama();
+	// individual element tween examples
+	//controller.addTween(250,
+	//	TweenMax.from($('#experiences'), 1, {opacity:0, ease:Power2.easeInOut}), false);
+
+	controller.addTween('a.scroll', TweenMax.from($('.header'), .7, {opacity:0, top:-200, ease:Power4.easeInOut}));
+	controller.addTween('a.scroll', TweenMax.to($('a.scroll'), .7, {opacity:0, ease:Power4.easeInOut}));
+
+});
+
+// parallax example
+	controller.addTween(
+	  '#examples-parallax',
+	  (new TimelineLite())
+	    .append([
+	      TweenMax.fromTo($('#parallax-it-left'), 1,
+	        {css:{top: 200}, immediateRender:true},
+	        {css:{top: -600}}),
+	      TweenMax.fromTo($('#parallax-it-right'), 1,
+	        {css:{top: 500}, immediateRender:true},
+	        {css:{top: -1250}})
+	    ]),
+	  1000 // scroll duration of tween
+	);
+
+/**************************************************
+
+				Animation for Experiences - RollOver
+
+**************************************************/
 
 $(".thumb a").each(function(index, element){
-  var tl = new TimelineLite({paused:true});
-  tl.to(element, 0.2, {opacity:1, ease:Power2.easeInOut})
-  tl.to($(element).find(".background"), 0.4, {backgroundColor:"#b4c23d", y:-105, ease:Power1.easeOut})
-  tl.to($(element).find(".headline"), 0.1, {y:-30, ease:Power1.easeIn}, '-=0.4')
-  tl.to($(element).find(".location"), 0.1, {y:-150, ease:Power1.easeOut}, '-=0.1')
+	var tl = new TimelineLite({paused:true});
+		bgHeight = $(element).find(".text-box").height()+25;
+
+  //tl.to(element, 0.2, {opacity:1, ease:Power2.easeInOut})
+  tl.to($(element).find(".background"), 0.3, {backgroundColor:"#b4c23d", height:bgHeight, ease:Power2.easeInOut})
+  tl.to($(element).find(".headline"), 0.3, {y:-30, ease:Power1.easeInOut}, '-=0.2')
+  tl.to($(element).find(".location"), 0.3, {y:-40, ease:Power1.easeInOut}, '-=0.2')
   element.animation = tl;
 })
 //toggle play and reverse of each .feature element's timeline on hover
@@ -55,32 +89,22 @@ $(".thumb a").hover(over, out);
 
 function over(){
   this.animation.play();
+  //alert(bgHeight);
 }
 
 function out(){
   this.animation.reverse();
 }
 
-/**************************************************
-
-					Parallax
-
-**************************************************/
-$(document).ready(function() {
-	var controller = $.superscrollorama();
-	// individual element tween examples
-	controller.addTween('#test', TweenMax.from( $('#test'), .5, {css:{opacity: 0}}));
-});
 
 /**************************************************
 
 					Resize H2
 
+//function to resize H2 title
 **************************************************/
 
-//TweenLite.to(".header h2", 1, {scale:0.5, ease:Elastic.easeOut, delay:2});
-
-var stage = $(".header h2");
+var stage = $(".cover h2");
 var stageParent = stage.parent();
 
 function scaleStage (){
@@ -96,9 +120,6 @@ if (rescale > 0.6 && rescale < 1 ) {
     scale:rescale
   });
 };
-//  TweenMax.set(stageParent, {
-//    height:stageHeight * rescale
-//  });
 
 //alert(rescale);
 
@@ -108,38 +129,3 @@ window.addEventListener('resize', scaleStage, false);
 window.addEventListener('orientationchange', scaleStage, false);
 //now call it immediately to scale it to the current screen
 scaleStage();
-
-
-/**************************************************
-
-					Scroll
-
-**************************************************/
-
-/* http://blog.bassta.bg/2013/05/smooth-page-scrolling-with-tweenmax/ */
-
-$(function(){
-
-	var $window = $(window);		//Window object
-
-	var scrollTime = 1;			//Scroll time
-	var scrollDistance = 170;  //Distance. Use smaller value for shorter scroll and greater value for longer scroll
-
-	$window.on("mousewheel DOMMouseScroll", function(event){
-
-		event.preventDefault();
-
-		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
-		var scrollTop = $window.scrollTop();
-		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
-
-		TweenMax.to($window, scrollTime, {
-			scrollTo : { y: finalScroll, autoKill:true },
-				ease: Power1.easeOut,	//For more easing functions see http://api.greensock.com/js/com/greensock/easing/package-detail.html
-				autoKill: true,
-				overwrite: 5
-			});
-
-	});
-
-});
